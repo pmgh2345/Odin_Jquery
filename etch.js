b83ad9d86306b5f06b9cd9aa448fa2a6ab2f128a
Paul Morgen <psmorgen@gmail.com>
2016-01-08 23:11:04 -0800

    fix false resets

--- etch.js
+++ etch.js
@@ -1,23 +1,25 @@
 $(document).ready(function() {
-	
-	var size = prompt("What should be the size of an area's side?");
-	var dimension = ($('.container').width())/size;
-	var dimensionA = dimension+'px';
-	console.log(dimensionA);
-	for (var counter = 1; counter < (size*size+1); counter++) {
-		$('.container').append('<div></div>');
-	}
-	$( 'div div').addClass('square');
-	$('.square').css({
+	var staging = function() {
+		var size = prompt("What should be the size of an area's side?");
+		var dimension = ($('.container').width())/size;
+		var dimensionA = dimension+'px';
+		console.log(dimensionA);
+		for (var counter = 1; counter < (size*size+1); counter++) {
+			$('.container').append('<div></div>');
+		}
+		$( 'div div').addClass('square');
+		$('.square').css({
 			"width":dimensionA,
 			"height":dimensionA
 		});
-	$('.square').on('mouseenter', function() {
-		$(this).removeClass('square');
-		$(this).addClass('squareA');
-	});
-	$('button').live('click',function() {
-		
+		$('.square').on('mouseenter', function() {
+			$(this).removeClass('square');
+			$(this).addClass('squareA');
+		});
+	};
+	staging();
+	$('button').on('click',function() {
+		staging();
 	});
 	
 });
